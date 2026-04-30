adc5c3e1291ebd1a4c0db49c3769d7d026c895be
Amulya Rawat <amulyarawat87@gmail.com>
2026-04-30 14:49:17 +0530

    Spring boot boiler plate code

---
+++ src/main/java/com/fileTransfer/FileTransferApplication/FileTransferApplication.java
@@ -0,0 +1,13 @@
+package com.fileTransfer.FileTransferApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileTransferApplication.class, args);
+	}
+
+}
