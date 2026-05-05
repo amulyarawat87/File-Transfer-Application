4cffa8696383f61e6ba7f7c7dc581b37422cd601
Amulya Rawat <amulyarawat87@gmail.com>
2026-05-06 01:41:48 +0530

    unnecessary code

--- src/main/java/FileTransferApplication/Service/FileService.java
+++ src/main/java/FileTransferApplication/Service/FileService.java
@@ -1,12 +1,10 @@
 package FileTransferApplication.Service;
 
 import FileTransferApplication.Model.FileMetadata;
-import FileTransferApplication.Repository.FileMetadataRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
