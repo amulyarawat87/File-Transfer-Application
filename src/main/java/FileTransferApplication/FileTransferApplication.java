78fa466b70abfb3f0e3cfbdd062819e17c1e6ebd
amulyarawat87 <amulyarawat87@gmail.com>
2026-06-17 08:04:20 +0000

    adding different environment files

--- src/main/java/FileTransferApplication/FileTransferApplication.java
+++ src/main/java/FileTransferApplication/FileTransferApplication.java
@@ -1,5 +1,6 @@
 package FileTransferApplication;
 
+import FileTransferApplication.Config.DotenvEnvironmentPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,7 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class FileTransferApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FileTransferApplication.class, args);
+		SpringApplication app = new SpringApplication(FileTransferApplication.class);
+		// Register the environment listener for dotenv loading
+		app.addListeners(new DotenvEnvironmentPostProcessor());
+		app.run(args);
 	}
 
 }
