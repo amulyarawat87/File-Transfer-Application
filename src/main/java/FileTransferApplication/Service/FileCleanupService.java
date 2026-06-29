a2dbd7a346a633d8687621fa3daef8e62652b0dd
amulyarawat87 <amulyarawat87@gmail.com>
2026-06-29 13:07:01 +0000

    Testing cleanup process

--- src/main/java/FileTransferApplication/Service/FileCleanupService.java
+++ src/main/java/FileTransferApplication/Service/FileCleanupService.java
@@ -18,14 +18,17 @@ public class FileCleanupService {
     @Autowired
     private S3Service s3;
 
-    private final long schedulerHours = 1;
+    private final long schedulerHours = 0;
 
     @Scheduled(fixedRate = schedulerHours * 60 * 60 * 1000)
     public void deleteExpiredFiles(){
         List<FileMetadata> files= fileMetadataRepo.findAll();
+        System.out.println("DEBUG: Running scheduled cleanup task. Total files in DB: " + files.size());
 
         for(FileMetadata file: files){
+            System.out.println("DEBUG: Checking file: " + file.getFileId());
             if(file.getExpiryDateTime().isBefore(LocalDateTime.now())) {
+                System.out.println("DEBUG: Deleting expired file: " + file.getFileId());
                 s3.deleteFile(file.getFileId());
                 fileMetadataRepo.delete(file);
             }
